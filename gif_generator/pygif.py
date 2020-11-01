6dd0f0754a1e4b0b2649c6506b9a358cdab035ca
julian <julianvicens@gmail.com>
2020-11-01 08:27:04 +0100

    gif generator

--- gif_generator/pygif.py
+++ gif_generator/pygif.py
@@ -5,16 +5,16 @@ n = 101
 with imageio.get_writer('movie'+str(n)+'.gif', mode='I') as writer:
 
 	for number in range(0,5,1):
-		filename = 'name/100/name-0000.png'
+		filename = '../name/100/name-0000.png'
 		image = imageio.imread(filename)
 		writer.append_data(image)
 
 	for number in range(0, n, 1):
-		filename = 'name/100/name-' + str(number).zfill(4) +'.png'
+		filename = '../name/100/name-' + str(number).zfill(4) +'.png'
 		image = imageio.imread(filename)
 		writer.append_data(image)
 
 	for number in range(n, 0, -1):
-		filename = 'name/100/name-' + str(number).zfill(4) +'.png'
+		filename = '../name/100/name-' + str(number).zfill(4) +'.png'
 		image = imageio.imread(filename)
 		writer.append_data(image) 
